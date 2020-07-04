0b0920c1937bbacc685c52af2187c867794cf4a9
SaltukArkali <67021783+SaltukArkali@users.noreply.github.com>
2020-07-04 16:20:34 +0200

    Update TestClass2.java

--- src/TestClass2.java
+++ src/TestClass2.java
@@ -1,5 +1,6 @@
 public class TestClass2 {
     public static void main(String[] args) {
         System.out.println("Git'den merhaba 2");
+        System.out.println("GitHub'dan hello 2");
     }
 }
